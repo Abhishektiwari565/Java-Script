4d9224edd4f8e4e3eaa5cec8aef85fdd56918124
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-04-10 23:24:55 +0530

    Add files via upload

---
+++ switch-case/script.js
@@ -0,0 +1,49 @@
+// wap to print month name by user choice of number using switch case.
+
+let month = parseInt(prompt("enter month"))
+
+switch (month) {
+    case 1:
+        document.writeln("january");
+        break;
+    case 2:
+        document.writeln("february");
+        break;
+    case 3:
+        document.writeln("march");
+        break;
+    case 4:
+        document.writeln("april");
+        break;
+    case 5:
+        document.writeln("may");
+        break;
+    case 6:
+        document.writeln("june");
+        break;
+    case 7:
+        document.writeln("july");
+        break;
+    case 8:
+        document.writeln("august");
+        break;
+    case 9:
+        document.writeln("september");
+        break;
+    case 10:
+        document.writeln("october");
+        break;
+    case 11:
+        document.writeln("november");
+        break;
+    case 12:
+        document.writeln("december");
+        break;
+        default:
+            document.writeln("invalid option ")
+           
+
+            
+        
+
+}
