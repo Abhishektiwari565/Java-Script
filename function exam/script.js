ef0a367b1cdf1bdb81ca42f12e10a7f7aae601ca
Abhishektiwari565 <abhishektiwari78563@gmail.com>
2025-05-09 15:19:09 +0530

    Add files via upload

---
+++ function exam/script.js
@@ -0,0 +1,31 @@
+// Q1 WAP TO FIND FACTORIAL USING RECURSION.
+
+// function factorial(n) {
+    
+//     if(n==1){
+//         return 1;
+//     }
+//      return n * factorial(n - 1);
+
+// }
+// document.writeln(factorial(5))
+
+
+// Q2 WAP TO CHECK WHETHER A NUMBER IS A PALINDROME OR NOT USING A FUNCTION.
+
+// function palindrome(num){
+//     let temp=num;
+//     let reverse=0;
+//     while(temp>0){
+//     let digit=temp%10;
+//     reverse=reverse*10+digit;
+//     temp=Math.floor(temp/10);
+//     }
+//     if(reverse==num){
+//         document.writeln("Given number is palindrome");
+//     }else{
+//         document.writeln("Given number is not palindrome");
+//     }
+
+// }
+// palindrome(121);
